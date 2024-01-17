842259e9ac030560e63f0a677ee904e1eba9abfd
ErMuthii <er.muthii@gmail.com>
2024-01-17 15:31:01 +0300

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = [0, 1]
+
+    for _ in range(length - 2):
+        c = fibonacci_sequence[-2] + fibonacci_sequence[-1]
+        fibonacci_sequence.append(c)
+
+    return fibonacci_sequence
+
+
+        
